cae3f191d743f89d188b5b3065ef03aa99964d68
Dovid Gefen <dovidgef@gmail.com>
2019-01-18 15:34:36 +0200

    Initial commit for bluetooth_unlock

---
+++ README.md
@@ -0,0 +1,15 @@
+### Prerequisites
+`sudo apt-get install libbluetooth-dev`
+
+### Install Pybluez
+pip install pybluez
+pip install bt-proximity
+
+
+
+### Lock system
+xdg-screensaver lock
+### Unlock system
+loginctl unlock-session
+### Turn on screen
+xset dpms force on
---
+++ bluetooth_unlock.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from rssi import BluetoothRSSI
+import time
+import yaml
+import os
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+bt_address = cfg['mac_address']
+interval = cfg['interval']
+max_rssi = cfg['max_rssi']
+locked = False
+last_values = []
+average_value = 0
+
+print("track device:", bt_address)
+btrssi = BluetoothRSSI(addr=bt_address)
+while True:
+    rssi = btrssi.request_rssi()
+    if rssi is not None:
+        rssi = rssi[0]
+    else:
+        print("lost connection")
+        rssi = -85
+
+    last_values.append(rssi)
+    print("signal strength:", rssi)
+    print(last_values)
+    if len(last_values) > interval:
+        last_values.pop(0)
+    average_value = sum(last_values) / len(last_values)
+    print("average:", average_value)
+
+
+    if average_value < max_rssi and not locked:
+        os.system('xdg-screensaver lock')
+        locked = True
+        time.sleep(3)
+    elif average_value >= max_rssi and locked:
+        os.system('loginctl unlock-session && xset dpms force on')
+        locked = False
+
+
+    time.sleep(.3)
